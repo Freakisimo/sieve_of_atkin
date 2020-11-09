47b677bbd9c382fc06c90524a8f54b13c02623fa
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-11-09 13:14:38 -0700

    Add package docs

--- mix.exs
+++ mix.exs
@@ -1,12 +1,18 @@
 defmodule SieveOfAtkin.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/Cantido/sieve_of_atkin"
+
   def project do
     [
+      name: "SieveOfAtkin",
+      description: "Generates prime numbers using the Sieve of Atkin algorithm.",
+      package: package(),
       app: :sieve_of_atkin,
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      source_url: @source_url,
       deps: deps()
     ]
   end
@@ -15,6 +21,15 @@ defmodule SieveOfAtkin.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Rosa Richter"],
+      licenses: ["MIT"],
+      links: %{"Github" => @source_url}
     ]
   end
 
